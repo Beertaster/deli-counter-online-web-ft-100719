6c50d35c1012cf619df06a30db2174f99c76e268
William Stevens <wstevens.hornet@gmail.com>
2019-10-12 05:46:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-   number = 1
+   counter = 1
     katz_deli.each do |customer|
-      line_positions << "#{number}. #{customer}"
-      number += 1
+      line_positions << "#{counter}. #{customer}"
+      counter += 1
     end
     puts "The line is currently: #{line_positions.join(" ")}"
   end
