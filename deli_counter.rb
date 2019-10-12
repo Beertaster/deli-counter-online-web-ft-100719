f3b1ba90e2d06ae136190c9dfb6992a3cc0c9a5a
William Stevens <wstevens.hornet@gmail.com>
2019-10-12 05:43:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,13 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty"
   else
-  arr = []
-  counter= 1
-  puts "The line is currently: "
-  katz_deli.each do |name|
-    put "#{counter}. #{name}"
-    counter += 1
+    arr = []
+    counter= 1
+    puts "The line is currently: "
+    katz_deli.each do |name|
+      put "#{counter}. #{name}"
+      counter += 1
+    end
   end
 end
 
